1058d57d5fb20f1f69c234b5e99ff10c885172f1
ethan-sorrell <esorrell@crimson.ua.edu>
2019-12-03 13:55:32 -0600

    day 3 part one

--- day03/soln.cpp
+++ day03/soln.cpp
@@ -1,16 +1,12 @@
 #include<vector>
 #include<fstream>
-#include<iostream>
-#include<string>
-#include<unordered_map>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <sstream>
+#include <set>
 
 using namespace std;
-
-struct LineSegment
-{
-    vector<int> from;
-    vector<int> to;
-};
 
 int main()
 {
@@ -20,30 +16,47 @@ int main()
     direction['R'] = {1, 0};
     direction['L'] = {-1, 0};
 
-    vector<LineSegment> segments;
-    vector<int> startP = {0, 0};
+    vector<int> lastP = {0, 0};
+    set<vector<int>> points;
 
-    // Read and parse line segments for first wire
     string token;
     fstream fs("input.txt");
-    bool done = false;
-    while (!done && getline(fs, token, ',')) {
-        if (token.back() == '\n')
-        {
-            done = true;
-            token.pop_back();
-        }
+    string wire1, wire2;
+    getline(fs, wire1);
+    getline(fs, wire2);
+    // Read and parse points for first wire
+    istringstream iss (wire1);
+    while (getline(iss, token, ',')) {
         int magnitude = stoi(token.substr(1));
-        LineSegment l;
-
-        l.from = startP;
-        startP[0] += direction[token[0]][0] * magnitude;
-        startP[1] += direction[token[0]][1] * magnitude;
-        l.to = startP;
-        segments.push_back(l);
+        vector<int> dir = direction[token[0]];
+        for (int i = 0; i < magnitude; i++) {
+            lastP = {lastP[0] + dir[0],
+                     lastP[1] + dir[1]};
+            points.insert(lastP);
+        }
     }
-    // Read and parse line segments for second wire
-    // Checking for intersection
 
+    // Read and parse points for second wire
+    // Checking for intersections
+    lastP = {0, 0};
+    int min_distance = -1;
+    istringstream iss2 (wire2);
+    while (getline(iss2, token, ',')) {
+        int magnitude = stoi(token.substr(1));
+        vector<int> dir = direction[token[0]];
+        for (int i = 0; i < magnitude; i++) {
+            lastP = {lastP[0] + dir[0],
+                     lastP[1] + dir[1]};
+            if (points.count(lastP)) {
+                if (min_distance == -1) {
+                    min_distance = abs(lastP[0]) + abs(lastP[1]);
+                } else {
+                    min_distance = (min_distance > (abs(lastP[0]) + abs(lastP[1]))) ?
+                        (abs(lastP[0]) + abs(lastP[1])) : min_distance;
+                }
+            }
+        }
+    }
+    cout << min_distance << endl;
     return 0;
 }
