209de508ed68ce67af7979ad88647d7864964f9a
ethan <ethan@debian>
2019-12-06 18:27:47 -0600

    day 5 part 1

--- day05/soln.rkt
+++ day05/soln.rkt
@@ -1,59 +1,49 @@
 #lang racket
 (require racket/format)
 
-;;  take a program and program counter
-;;  return a program and program counter
-
-(define (paramMode instr num)
-  (if (> (+ 2 num) (string-length instr)) 0
-      (string-ref instr (- (string-length instr) (+ 2 num)))))
-
-(define (getParam instr num pc tokens)
+(define (get-param instr num pc tokens)
   (let* ([param (vector-ref tokens (+ pc num))]
-         [paramMode (paramMode instr num)])
+         [param-mode (remainder (quotient instr (expt 10 (add1 num))) 10)])
     (cond
-      [(equal? #\1 paramMode) param]
-      [(equal? #\0 paramMode) (vector-ref tokens (string->number param))]
-      [else (begin (println (~a "Error: incorrect paramMode in instr "
+      [(= 1 param-mode) param]                     ;; immediate mode
+      [(= 0 param-mode) (vector-ref tokens param)] ;; position mode
+      [else (begin (println (~a "Error: incorrect param-mode in instr "
                                 instr " at " pc)) 0)])))
 
-;; (define (getParam instr num pc tokens)
-;;   (println pc)
-;;   (println num)
-;;   (println (+ pc num)))
-
-(define (runInstr state)
+(define (run-instr state)
   (let* ([pc (vector-ref state 0)]
          [tokens (vector-ref state 1)]
-         [instr (~a (vector-ref tokens pc)
-                    #:align 'right
-                    #:min-width 5
-                    #:left-pad-string (~a "0"))]
-         [op (substring instr (- (string-length instr) 2))]
-         [param1 (string->number (getParam instr 1 pc tokens))]
-         [param2 (string->number (getParam instr 2 pc tokens))]
-         [param3 (string->number (getParam instr 3 pc tokens))])
+         [instr (vector-ref tokens pc)]
+         [op (remainder instr 100)])
     (cond
-      [(= (string->number op) 1)
-       (begin (vector-set! tokens param3 (number->string (+ param1 param2)))
-              (vector (+ pc 4) tokens))]
-      [(= (string->number op) 2)
-       (begin (vector-set! tokens param3 (number->string (* param1 param2)))
-              (vector (+ pc 4) tokens))]
-      [(= (string->number op) 3)
-       (begin (vector-set! tokens param1 (number->string param1))
-              (vector (+ pc 2) tokens))]
-      [(= (string->number op) 4) (begin (println (vector-ref tokens param1))
-                       (vector (+ pc 2) tokens))]
-      [(= (string->number op) 99) tokens]
-      [else (begin (println (~a "Error Unknown Op " op)))])))
+      [(= op 1)
+       (let ([param1 (get-param instr 1 pc tokens)]
+             [param2 (get-param instr 2 pc tokens)]
+             [param3 (vector-ref tokens (+ pc 3))])
+         (vector-set! tokens param3 (+ param1 param2))
+         (vector (+ pc 4) tokens))]
+      [(= op 2)
+       (let ([param1 (get-param instr 1 pc tokens)]
+             [param2 (get-param instr 2 pc tokens)]
+             [param3 (vector-ref tokens (+ pc 3))])
+         (vector-set! tokens param3 (* param1 param2))
+         (vector (+ pc 4) tokens))]
+      [(= op 3)
+       (begin
+         (vector-set! tokens (vector-ref tokens (add1 pc)) 1)
+         (vector (+ pc 2) tokens))]
+      [(= op 4) (begin
+                  (println (get-param instr 1 pc tokens))
+                  (vector (+ pc 2) tokens))]
+      [(= op 99) tokens]
+      [else (begin (println (~a "Error Unknown Op " op
+                                " pc " pc)))])))
 
 
 (let* ([program (file->string "input.txt")]
-       [program (substring program 0 (- (string-length program) 1))]
-       [tokens (list->vector (string-split program ","))])
+       [program (substring program 0 (- (string-length program) 1))] ;; remove trailing newline
+       [tokens (list->vector (string-split program ","))]
+       [tokens (vector-map string->number tokens)])
   (let loop ((state (vector 0 tokens)))
-    (if (not (vector? state)) state
-        (loop (runInstr state))
-        ;; (runInstr state)
-        )))
+    (if (not (= 2 (vector-length state))) state
+        (loop (run-instr state)))))
