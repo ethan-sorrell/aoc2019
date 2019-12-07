0210bdab008a5a0eb5ece1b807c8cee52ab79b34
ethan <esorrell@crimson.ua.edu>
2019-12-07 11:33:40 -0600

    day 6 part 2

--- day06/soln.rkt
+++ day06/soln.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require racket/set)
+
 (define (add-orbit phrase orbits)
   (let ([object (first phrase)]
         [satellite (second phrase)])
@@ -19,9 +21,43 @@
           (+ upstream
              (apply + (map (lambda (x) (loop x (add1 upstream))) satellites)))))))
 
+(define (parse-dlink tokenlists)
+  (let loop ((rem tokenlists)
+             (acc (hash)))
+    (if (null? rem) acc
+        (loop (rest rem) (add-orbit (first rem) (add-satellite (first rem) acc))))))
+
+(define (add-satellite phrase orbits)
+  (let ([object (second phrase)]
+        [satellite (first phrase)])
+    (hash-update orbits object (lambda (x) (cons satellite x)) '())))
+
+(define (unvisited-adjacent table parents visited)
+  (let loop ((acc (set))
+             (rem parents))
+    (if (set-empty? rem) (set-subtract acc visited)
+        (loop
+         (set-union acc (list->set (hash-ref table (set-first rem) '())))
+         (set-rest rem)))))
+
+;; current-level list of nodes
+;; check if target is in current-level
+;; otherwise, add all unvisited from current level
+;; need to maintain: current-level, visited, distance
+
+(define (min-distance table from to)
+  (let loop ((current-level (set from))
+             (visited (set))
+             (distance 0))
+    (if (set-member? current-level to) distance
+        (loop
+         (unvisited-adjacent table current-level visited)
+         (set-union visited current-level)
+         (add1 distance)))))
+
 (let* ([lines (file->lines "input.txt")]
        [tokenlists (map (lambda (x) (string-split x ")")) lines)]
-       [orbits (parse-orbits tokenlists)])
-  (calculate-orbits orbits)
-  ;; orbits
-  )
+       [orbits (parse-orbits tokenlists)]
+       [dlink-table (parse-dlink tokenlists)])
+  (println (calculate-orbits orbits))
+  (println (- (min-distance dlink-table "SAN" "YOU") 2)))
