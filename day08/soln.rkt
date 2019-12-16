056f80913ab68d7cc90fd521c2e423f1f6fd4db3
ethan-sorrell <esorrell@crimson.ua.edu>
2019-12-15 23:45:48 -0600

    day 8: minor clean up

--- day08/soln.rkt
+++ day08/soln.rkt
@@ -12,24 +12,22 @@
          [layer (argmin (lambda (x) (length (filter (lambda (y) (equal? y #\0))
                                                     (string->list x))))
                         layers)])
-    (*
-     (length (filter (lambda (x) (equal? #\2 x)) (string->list layer)))
-     (length (filter (lambda (x) (equal? #\1 x)) (string->list layer))))))
+    (* (length (filter (lambda (x) (equal? #\2 x)) (string->list layer)))
+       (length (filter (lambda (x) (equal? #\1 x)) (string->list layer))))))
 
 (define (layers->positions layers)
   (map list->string
        (for/list ([i (range (string-length (first layers)))])
          (map (lambda (x) (string-ref x i)) layers))))
 
-(define (occlude layer)
-  (findf (lambda (x) (< (char->integer x) 50)) (string->list layer)))
+(define (occlude layers)
+  (findf (lambda (x) (< (char->integer x) 50)) (string->list layers)))
 
 (define (solve2)
   (let* ([file (file->string "input.txt")]
          [file (substring file 0 (- (string-length file) 1))]
          [layers (for/list [(i (range (quotient (string-length file) (* 25 6))))]
                    (substring file (* 25 6 i) (* 25 6 (+ 1 i))))]
-
          [positions (layers->positions layers)]
          [top (list->string
                (map (lambda (x) (if (equal? x #\0) #\_ x))
